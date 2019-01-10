e31294c4ed421074c44deb748eb83256a093b798
Mike Mbugua <Mikembugua76@gmail.com>
2019-01-10 06:16:30 -0800

    [feature #163070921] Register meetup view blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from .api.v1.views.user_views import user_version1
+from .api.v1.views.meetup_views import meetup_version1
 
      
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(user_version1, url_prefix='/api/v1/')
+    app.register_blueprint(meetup_version1, url_prefix='/api/v1/')
     
     return app  
+
