bc41adf0c33a04ec694fbe100f7a49249d9a4c27
Michael Mbugua <michaelngigi76@gmail.com>
2019-01-13 23:02:34 +0300

    [starts #163179375] Commit all previous edits in the view and model files

--- app/tests/v1/test_meetup_views.py
+++ app/tests/v1/test_meetup_views.py
@@ -3,9 +3,10 @@ import json
 import unittest
 from ... import create_app
 
+
 class TestMeetupEndPoints(unittest.TestCase):
     """Class that handles All test cases for meetup endpoints"""
-    
+
     def setUp(self):
         """Code to be excecuted before each test"""
         self.app = create_app()
@@ -17,40 +18,34 @@ class TestMeetupEndPoints(unittest.TestCase):
             "createdOn": "25th Dec 2018",
             "location": "Taj",
             "images": ["Food.jpg", "Kitchen.jpg"],
-            "topic": "Making Pasta", 
+            "topic": "Making Pasta",
             "happeningOn": "2nd Jan 2019, 09:40AM",
             "tags": ["Art", "Homestudy"]
-    }
-
+        }
 
     def test_create_meetup(self):
         """Test for creating a new meetup"""
         response = self.app.post('api/v1/meetups',
-                                data = json.dumps(self.data), 
-                                content_type="application/json")
-        
-    
-        
-        self.assertEqual(response.status_code, 201)
+                                 data=json.dumps(self.data),
+                                 content_type="application/json")
 
+        self.assertEqual(response.status_code, 201)
 
     def test_retrieve_meetups(self):
             """Test for retrieving all meetups"""
             response = self.app.get('api/v1/meetups',
-                                    data = json.dumps(self.data), 
+                                    data=json.dumps(self.data),
                                     content_type="application/json")
 
-            
             self.assertEqual(response.status_code, 200)
-    
+
     def test_retrieve_one_meetup(self):
         """Test for retrieving all meetups"""
         response = self.app.get('api/v1/meetups/1',
-                                data = json.dumps(self.data), 
+                                data=json.dumps(self.data),
                                 content_type="application/json")
-        
+
         self.assertEqual(response.status_code, 200)
-    
 
     def tearDown(self):
         """ Destroys set up data before running each test """
@@ -59,5 +54,3 @@ class TestMeetupEndPoints(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
- 
-
