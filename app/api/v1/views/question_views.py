bf5cdc19d83fe94fecbf84f7e15d0626b4589b72
Michael Mbugua <michaelngigi76@gmail.com>
2019-01-15 11:39:42 +0300

    [finishes #163072923]Add final validations to the questions view

--- app/api/v1/views/question_views.py
+++ app/api/v1/views/question_views.py
@@ -1,4 +1,5 @@
 """question views File"""
+
 from flask import Blueprint, make_response, jsonify, request
 from flask_restful import Resource
 
@@ -7,158 +8,141 @@ from app.api.v1.models.question_models import QuestionsModel
 
 class QuestionList(Resource, QuestionsModel):
     """Class for managing entire Questions List"""
+
     def __init__(self):
         self.db = QuestionsModel()
-
 
     def post(self):
         """Method for Creating a new question"""
 
         data = request.get_json()
 
-        if isinstance(data["createdOn"], str):
-            if  data["createdOn"] != "":
-                createdOn = data["createdOn"]
-            else:
-                return make_response(jsonify({
-                    'status':404,
-                    'error':"Please enter some value at created on"
-                }),404)
-        else:
-            return make_response(jsonify({
-                    'status':404,
-                    'error':"Please enter a String for createdOn"
-            }),404)
-
         if isinstance(data["createdBy"], str):
-            if  data["createdBy"] != "":
+            if data["createdBy"] != "":
                 createdBy = data["createdBy"]
             else:
                 return make_response(jsonify({
-                    'status':404,
-                    'error':"CreatedBy cannot be left empty"
-                }),404)
+                    'status': 404,
+                    'error': "CreatedBy cannot be left empty"
+                }), 404)
         else:
             return make_response(jsonify({
-                    'status':404,
-                    'error':"Please enter an String for CreatedBy"
-            }),404)        
+                'status': 404,
+                'error': "Please enter an String for CreatedBy"
+            }), 404)
 
         if isinstance(data["meetup"], int):
-            if  data["meetup"] != "":
+            if data["meetup"] != "":
                 meetup = data["meetup"]
             else:
                 return make_response(jsonify({
-                    'status':404,
-                    'error':"Please enter some value for meetup"
-                }),404)
+                    'status': 404,
+                    'error': "Please enter some value for meetup"
+                }), 404)
         else:
             return make_response(jsonify({
-                    'status':404,
-                    'error':"Please enter an Integer for meetup"
-            }),404)        
+                'status': 404,
+                'error': "Please enter an Integer for meetup"
+            }), 404)
 
         if isinstance(data["title"], str):
-            if  data["title"] != "":
+            if data["title"] != "":
                 title = data["title"]
             else:
                 return make_response(jsonify({
-                    'status':404,
-                    'error':"Please enter some value for title"
-                }),404)
+                    'status': 404,
+                    'error': "Please enter some value for title"
+                }), 404)
         else:
             return make_response(jsonify({
-                    'status':404,
-                    'error':"Please enter a String for Title"
-            }),404)        
+                'status': 404,
+                'error': "Please enter a String for Title"
+            }), 404)
 
         if isinstance(data["body"], str):
-            if  data["body"] != "":
+            if data["body"] != "":
                 body = data["body"]
             else:
                 return make_response(jsonify({
-                    'status':404,
-                    'error':"Please enter some value for the Body"
-                }),404)
+                    'status': 404,
+                    'error': "Please enter some value for the Body"
+                }), 404)
         else:
             return make_response(jsonify({
-                    'status':404,
-                    'error':"Please enter a String for the body"
-            }),404)        
-        
+                'status': 404,
+                'error': "Please enter a String for the body"
+            }), 404)
+
         if isinstance(data["votes"], int):
-            if  data["votes"] != "":
+            if data["votes"] != "":
                 votes = data["votes"]
             else:
                 return make_response(jsonify({
-                    'status':404,
-                    'error':"Please enter some value for votes"
-                }),404)
+                    'status': 404,
+                    'error': "Please enter some value for votes"
+                }), 404)
         else:
             return make_response(jsonify({
-                    'status':404,
-                    'error':"Please enter an Integer for votes"
-            }),404)
+                'status': 404,
+                'error': "Please enter an Integer for votes"
+            }), 404)
 
-        resp = self.db.add_question(createdOn, createdBy, meetup, title, body, votes)
+        resp = self.db.add_question(createdBy, meetup, title, body, votes)
 
         return make_response(jsonify({
-            "All questions": resp,
-            "data":{
-                'Status': "Okay",
-                'Message': "Question Posted Successfully"
-            }
+            'Status': 201,
+            "Data": resp,
+            'Message': "Question Posted Successfully"
+            
         }), 201)
 
-                
-    
     def get(self):
         """Return all questions"""
         all_questions = self.db.get_all_questions()
-        
+
         return make_response(jsonify({
-            "All questions": all_questions,
-            "data":{
-                'Status': "Okay",
-                'Message': "Success"
-            }
-            }), 200)
+            'Status': 200,
+            "Data": all_questions,
+            'Message': "Success"            
+        }), 200)
 
 
 class Question(Resource, QuestionsModel):
     """Class for managing a single question"""
+
     def __init__(self):
         self.db = QuestionsModel()
 
-    
     def get(self, questionId):
         """Return one question"""
         one_question = self.db.get_one_question(questionId)
         if not one_question:
             return make_response(jsonify({
-                    'status':404,
-                    'error':"Question does not exist"
-                }),404)
+                'status': 404,
+                'error': "Question does not exist"
+            }), 404)
         return make_response(jsonify({
-                "Question": one_question
-            }), 200)
-
+            "Status": 200,
+            "Question": one_question
+        }), 200)
 
     # Upvote a Question
+
     def patch(self, questionId):
         "Upvote or downvote a question"
 
         question = self.db.upvote(questionId)
         if not question:
             return {
-                "Error": "Question does not exist",
-                "status": 404
+                "status": 404,
+                "Message": "Question does not exist"
             }, 404
         return {
-            
-            "message": "Upvote Successful",
-            "Question": question
+            "Status": 200,
+            "Data": question,
+            "message": "Upvote Successful"
         }, 200
+
 
 class QuestionDownvote(Resource, QuestionsModel):
     def __init__(self):
@@ -171,14 +155,11 @@ class QuestionDownvote(Resource, QuestionsModel):
         question = self.db.downvote(questionId)
         if not question:
             return {
-                "Error": "Question does not exist",
-                "status": 404
+                "status": 404,
+                "Message": "Question does not exist"
             }, 404
-        return {            
-            "message": "Upvote Successful",
-            "Question": question
+        return {
+            "status": 200,
+            "Data": question,
+            "message": "Upvote Successful"
         }, 200
-
-
-
-
--- app/tests/v1/test_question_views.py
+++ app/tests/v1/test_question_views.py
@@ -10,39 +10,56 @@ class TestquestionEndPoints(unittest.TestCase):
     def setUp(self):
         """Code to be excecuted before each test"""
         self.app = create_app()
+        self.client = self.app.test_client()
+        self.app_context = self.app
         self.app.testing = True
-        self.app = self.app.test_client()
 
         self.data = {
-            "questionId": 1,
-            "createdOn": "15th Jan 2014",
-            "createdBy": 5,
-            "question": 18,
-            "title":   "Andela Bootcamp",
-            "body":   "This is an Andela bootcamp meeting",
-            "votes": 24,
-        }
+	            "createdBy": "Michael Mbugua",
+                "meetup": 1,
+                "title":   "Bourne Legacy",
+                "body":   "So how often will we have to meet?",
+                "votes": 5
+            }
 
-        
-
-    def test_create_question(self):
+    def test_add_question(self):
         """Test for creating a new question"""
-        response = self.app.post('api/v1/questions',
+        response = self.client.post('api/v1/questions',
                                  data=json.dumps(self.data),
                                  content_type="application/json")
 
-        # result = json.loads(response.data)
-
-        # self.assertEqual(result["title"], "Andela Bootcamp")
         self.assertEqual(response.status_code, 201)
 
-    def test_retrieve_questions(self):
+    def test_get_all_questions(self):
             """Test for retrieving all questions"""
-            response = self.app.get('api/v1/questions',
+            response = self.client.get('api/v1/questions',
                                     data=json.dumps(self.data),
                                     content_type="application/json")
 
             self.assertEqual(response.status_code, 200)
+
+    def test_get_one_question(self):
+            """Test for retrieving all questions"""
+            response = self.client.get('api/v1/questions/1',
+                                    data=json.dumps(self.data),
+                                    content_type="application/json")
+
+            self.assertEqual(response.status_code, 201)
+
+    def test_upvote(self):
+            """Test for retrieving all questions"""
+            response = self.client.patch('api/v1/questions/1',
+                                      data=json.dumps(self.data),
+                                      content_type="application/json")
+
+            self.assertEqual(response.status_code, 404)
+
+    def test_downvote(self):
+        """Test for retrieving all questions"""
+        response = self.client.patch('api/v1/questions/1',
+                                  data=json.dumps(self.data),
+                                  content_type="application/json")
+        self.assertEqual(response.status_code, 404)
 
     def tearDown(self):
         """ Destroys set up data before running each test """
